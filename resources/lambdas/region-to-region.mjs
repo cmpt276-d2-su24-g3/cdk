9d2bb3ae7c288c402b33976f237777814ce0f150
Alex Noh <90161063+ndh707@users.noreply.github.com>
2024-07-02 20:29:33 -0700

    Refacted lambda function for readability

--- resources/lambdas/region-to-region.mjs
+++ resources/lambdas/region-to-region.mjs
@@ -2,36 +2,34 @@ import { Socket } from 'net'
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { PutCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
-// automatically tries to PUT in their own regions if not specified
-const client = new DynamoDBClient({ region: 'us-west-2' });
+// find way to get regions programmatically
+// once the lambda function is deployed, it loses ability to references the rest of this codebase
+const REGIONS = [
+  'ca-west-1',
+  'ca-central-1',
+  'us-west-1',
+  'us-west-2',
+  'us-east-1',
+  'us-east-2',
+];
+const DB_REGION = 'us-west-2';
+const TABLE_NAME = process.env.TABLE_NAME;
+const THIS_REGION = process.env.THIS_REGION;
+const TIME_TO_LIVE = 7 * 24 * 60 * 60; // 1 week in seconds
+
+const client = new DynamoDBClient({ region: DB_REGION });
 const ddbDocClient = DynamoDBDocumentClient.from(client);
 
-// find way to get regions programmatically
-const REGIONS = [
-    "ca-west-1",
-    "ca-central-1",
-    "us-west-1",
-    "us-west-2",
-    "us-east-1",
-    "us-east-2",
-]
-
-
-var count;
 export const handler = async () => {
-    count = 0;
-    console.log('Pinging All Regions')
     for (const region of REGIONS) await pingRegion(region);
     return {
         statusCode: 200,
-        body: `${count} Pings Complete`,
+        body: `Pings Complete`,
     };
 }
   
 async function pingRegion(region) {
     const url = `dynamodb.${region}.amazonaws.com`
-    console.log("Pinging " + region);
-  
     const client = new Socket()
     const start = process.hrtime.bigint()
     await new Promise((resolve, reject) => {
@@ -45,23 +43,30 @@ async function pingRegion(region) {
     const latency = Number(end - start) / 1e6
 
     try {
-        const time = new Date().toISOString();
-        const expireAt = Math.floor((new Date().getTime()/1000 + 7 * 24 * 60 * 60));
-        await ddbDocClient.send(new PutCommand({
-            TableName: process.env.TABLE_NAME,
-            Item: {
-                timestamp: time,
-                origin: process.env.THIS_REGION,
-                destination: region,
-                'destination#timestamp': region + '#' + time,
-                expireAt: expireAt,
-                latency: latency,
-            },
-        }));
-        console.log(region + " pinged successfully"); // console logs to cloudwatch
-        count++;
+        storeResult(region, latency)
+        console.log(region + " pinged successfully");
     } catch (error) {
         console.log(region + " ping failed");
         console.log(error);
     }
 }
+
+const storeResult = async (region, latency) => {
+    const currentDate = new Date();
+    const currentTimeInSeconds = Math.floor(currentDate.getTime()/1000);
+    const expireAt = currentTimeInSeconds + TIME_TO_LIVE;
+    const params = {
+        TableName: TABLE_NAME,
+        Item: {
+            timestamp: currentDate.toISOString(),
+            origin: THIS_REGION,
+            destination: region,
+            'destination#timestamp': `${region}#${currentDate.toISOString()}`,
+            expireAt: expireAt,
+            latency: latency,
+        },
+    };
+  
+    await ddbDocClient.send(new PutCommand(params));
+};
+  
