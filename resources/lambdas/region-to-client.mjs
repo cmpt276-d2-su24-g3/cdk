a110fd98e5c62646cd261dc29336740ec395b7a1
reinesana <17shana2017@gmail.com>
2024-10-01 23:34:41 -0700

    fixed cors issue

--- resources/lambdas/region-to-client.mjs
+++ resources/lambdas/region-to-client.mjs
@@ -2,9 +2,13 @@ import { Socket } from 'net';
 import { promises as dns } from 'dns';
 
 const headers = {
+  "Access-Control-Allow-Origin" : "*",
+  "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
   "Access-Control-Allow-Credentials": true,
   "Content-Type": "application/json",
 };
+
+
 
 export const handler = async (event) => {
   let body;
